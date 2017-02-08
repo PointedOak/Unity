c47808652c3faf3371036a493823f20076b90557
PointedOak <Jason.r.fidler@gmail.com>
2017-02-07 22:30:06 -0700

    FIGURED OUT GIT
    After weeks of doing the work but having issues with Git I have finally uploaded all i've worked on

---
+++ Assets/movesInvert.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movesInvert : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            gameObject.transform.Translate(.25f, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            gameObject.transform.Translate(-.25f, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            gameObject.transform.Translate(0, 0, -0.25f);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            gameObject.transform.Translate(0, 0, 0.25f);
+        }
+    }
+}
---
+++ Assets/rotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour {
+    public float speed;
+    public float rotx =15;
+    public float roty = 30;
+    public float rotz = 45;
+    // Use this for initialization
+    void Start () {
+        speed = Random.Range(.5f, 2.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate (new Vector3(rotx, roty, rotz) * Time.deltaTime * speed);
+	}
+}
